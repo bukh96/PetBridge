e6e50f1d30fe9f4402c934e969a47b2d1cceb763
bukh96 <alexbukharov159@gmail.com>
2025-06-09 19:01:49 +0200

    Initial commit

---
+++ src/main/java/com/example/PetBridge/PetBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetBridge/PetBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
