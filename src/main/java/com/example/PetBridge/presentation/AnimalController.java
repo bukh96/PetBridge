a2f79d831d808fae25a30a586e456fcaf7439ac3
bukh96 <alexbukharov159@gmail.com>
2025-06-22 20:25:35 +0200

    Add controllers with mapping

--- src/main/java/com/example/PetBridge/presentation/AnimalController.java
+++ src/main/java/com/example/PetBridge/presentation/AnimalController.java
@@ -1,4 +1,19 @@
 package com.example.PetBridge.presentation;
 
+import com.example.PetBridge.model.Animal;
+import com.example.PetBridge.businesslayer.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/animals")
+@RequiredArgsConstructor
+@CrossOrigin
 public class AnimalController {
+    private final AnimalService animalService;
+
+    @PostMapping
+    public Animal createAnimal(@RequestBody Animal animal) {
+        return animalService.save(animal);
+    }
 }
--- src/main/java/com/example/PetBridge/presentation/UserController.java
+++ src/main/java/com/example/PetBridge/presentation/UserController.java
@@ -1,4 +1,37 @@
 package com.example.PetBridge.presentation;
 
+import com.example.PetBridge.model.Animal;
+import com.example.PetBridge.model.User;
+import com.example.PetBridge.businesslayer.AnimalService;
+import com.example.PetBridge.businesslayer.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@CrossOrigin
 public class UserController {
+    private final UserService userService;
+    private final AnimalService animalService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    // Get matched animals for a specific user ID
+    @GetMapping("/{id}/match")
+    public List <Animal> getMatches(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(animalService::matchAnimals)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        
+    }
+
+
+
 }
